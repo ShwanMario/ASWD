42933b809880fe4664994867281baac13e5bce63
Xiongjie Chen <34535872+ShwanMario@users.noreply.github.com>
2021-10-06 11:44:40 +0100

    Update TransformNet.py

--- colour_transferring/TransformNet.py
+++ colour_transferring/TransformNet.py
@@ -6,11 +6,11 @@ class Mapping(nn.Module):
     def __init__(self, size):
         super(Mapping, self).__init__()
         self.size = size
-        self.net = nn.Sequential(nn.Linear(self.size, self.size))#),nn.Tanh(),nn.Linear(self.size, self.size), nn.Tanh(),nn.Linear(self.size,self.size))
+        self.net = nn.Sequential(nn.Linear(self.size, self.size), nn.ReLU())#),nn.Tanh(),nn.Linear(self.size, self.size), nn.Tanh(),nn.Linear(self.size,self.size))
 
     def forward(self, inputs):
         outputs = self.net(inputs)
-        print(inputs.abs().mean().item(),outputs.abs().mean().item())
+        #print(inputs.abs().mean().item(),outputs.abs().mean().item())
         return torch.cat((inputs, outputs), dim=-1)  # /torch.sqrt(torch.sum(out**2,dim=1,keepdim=True))
 
 
